953ba8c8a7546335427c6b518881a29c571d0d79
Brian Muller <brian.muller@livingsocial.com>
2011-04-15 14:41:27 -0400

    added unix liberal timestamp

---
+++ src/UnixLiberalTimestamp.java
@@ -0,0 +1,21 @@
+package com.livingsocial.hive.udf;
+
+import org.apache.hadoop.hive.ql.udf.UDFUnixTimeStamp;
+
+import org.apache.hadoop.hive.ql.exec.description;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+@description(
+	     name = "unix_liberal_timestamp",
+	     value = "_FUNC_(str) - gets unix timestamp in either yyyy-MM-dd HH:mm:ss or yyyy-MM-dd format",
+	     extended = "Example:\n" +
+	     "  > SELECT a.* FROM srcpart a WHERE _FUNC_ (a.hr) < unix_timestamp() LIMIT 1;\n"
+	     )
+public class UnixLiberalTimestamp extends UDFUnixTimeStamp {
+    public LongWritable evaluate(Text datestring) {
+	if(datestring.find(" ") == -1)
+	    datestring = new Text(datestring.toString() + " 00:00:00");
+	return super.evaluate(datestring);
+    }
+}
